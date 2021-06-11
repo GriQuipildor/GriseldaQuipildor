dfdb8dcc0043d5a6ec03158d734b23c08d03e41e
Griselda Quipildor <gri.quipildor@gmail.com>
2021-06-11 14:56:15 -0300

    Fetch API

---
+++ fetchApi/api.js
@@ -0,0 +1,12 @@
+var contenido = document.querySelector("#contenido")
+function traer(){
+    fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(function(data){
+        console.log(data.results["0"])
+        contenido.innerHTML = "Datos usuario: <br>"
+        contenido.innerHTML += "<img src='"+data.results["0"].picture.large+"'><br><br>"
+        contenido.innerHTML += "<p>Nombre: "+data.results["0"].name.first
+        contenido.innerHTML += "<p>Correo electronico: "+data.results["0"].email
+    })
+}
