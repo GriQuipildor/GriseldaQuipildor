37f316abf3a4d7e10cd8908e61701bdbc4d10b16
Ipperi Tomas <ipperitomas@gmail.com>
2021-06-18 18:42:27 -0300

    ejercicio13

---
+++ ejercicio13/maps.js
@@ -0,0 +1,32 @@
+var contenido = document.querySelector("#contenido")
+function traer(){
+    fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(function(data){
+        // console.log("muestro resultado=>",data.results[0].location.coordinates.latitude)
+        contenido.innerHTML = "Datos usuario: <br>"
+        contenido.innerHTML += "<img src='"+data.results[0].picture.large+"'><br><br>"
+        contenido.innerHTML += "<p>Nombre: "+data.results[0].name.first
+        contenido.innerHTML += "<p>Correo electronico: "+data.results[0].email
+        contenido.innerHTML += "<p>Latitud: "+data.results[0].location.coordinates.latitude
+        contenido.innerHTML += "<p>Longitud: "+data.results[0].location.coordinates.longitude
+    })
+}
+// "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+// const tilesProvider = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+// let myMap = L.maps('myMaps').setView([51.505, -0.09], 13)
+
+function mostrarMapa(){
+    var tilesProvider = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    let myMap = L.maps('myMaps').setView([51.505, -0.09], 13);
+
+    L.tileLayer(tilesProvider, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+   
+  
+}).addTo(myMaps);
+
+//let myMap = L.maps('myMaps').setView([51.505, -0.09], 13)
+}
