d28924d34239449facbd328d715153b87c927846
Ipperi Tomas <ipperitomas@gmail.com>
2021-06-18 18:50:21 -0300

    ejercicio13

--- ejercicio13/maps.js
+++ ejercicio13/maps.js
@@ -19,14 +19,14 @@ function traer(){
 
 function mostrarMapa(){
     var tilesProvider = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-    let myMap = L.maps('myMaps').setView([51.505, -0.09], 13);
+    let myMap = L.map('myMaps').setView([-32.9980772,-58.5201163], 13);
 
     L.tileLayer(tilesProvider, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
    
   
-}).addTo(myMaps);
-
+    }).addTo(myMap);
+    var marker = L.marker([-32.9980772,-58.5201163]).addTo(myMap);
 //let myMap = L.maps('myMaps').setView([51.505, -0.09], 13)
 }
