0b6e48dc53e5621761070e5f351f2fa44f884284
Griselda Quipildor <gri.quipildor@gmail.com>
2021-06-17 23:53:22 -0300

    Ordene los archivos

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Griselda Quipildor 
 gri.quipildor@gmail.com 
+
+Ejercicio 8: JS
+Ejercicio 10: AJAX
+Ejercicio 11: Fetch
+Ejercicio 12: Fetch API
--- ejercicio12/api.js
+++ ejercicio12/api.js
@@ -3,7 +3,7 @@ function traer(){
     fetch("https://randomuser.me/api/")
     .then(res => res.json())
     .then(function(data){
-        console.log(data.results["0"])
+        console.log(data.results[0].email)
         contenido.innerHTML = "Datos usuario: <br>"
         contenido.innerHTML += "<img src='"+data.results["0"].picture.large+"'><br><br>"
         contenido.innerHTML += "<p>Nombre: "+data.results["0"].name.first
