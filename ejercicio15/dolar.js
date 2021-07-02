a92fc8f150dc52abbea3d65ef3bd38b1a835f4ef
Ipperi Tomas <ipperitomas@gmail.com>
2021-07-02 18:26:46 -0300

    ejercicio15

--- ejercicio15/dolar.js
+++ ejercicio15/dolar.js
@@ -1,8 +1,18 @@
-function mostrarDolarOficial(){
-    fetch("https://api-dolar-argentina.herokuapp.com/api/dolaroficial")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
+// function mostrarDolarOficial(){
+//     fetch("https://api-dolar-argentina.herokuapp.com/api/dolaroficial")
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//     })
     
+// }
+
+
+function mostrarDolarBlue(){
+    var url = "https://api-dolar-argentina.herokuapp.com/api/dolarblue";
+  $.getJSON( url)
+    .done(function( data ) {
+        console.log(data)
+      });
+console.log("hola")
 }
